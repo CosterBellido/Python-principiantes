6d256ba24896cc9dfb88f0d97cdf0385bf7f16b4
CosterBellido <50610410+CosterBellido@users.noreply.github.com>
2021-01-19 14:56:06 -0500

    Nombre y dominio
    Muy facil de implementar

--- Rebanador de correo electronico.py
+++ Rebanador de correo electronico.py
@@ -1,10 +1,6 @@
 # Rebanador de correo electronico con python
-
 email = input('Entregue su correo: ').strip()
 username = email[:email.index('@')]
 domain_name = email[email.index('@')+1:]
 format_ = (f'Tu nombre de usuario es: "{username}" y tu dominio es "{domain_name}"')
 print(format_)
-
-
-
